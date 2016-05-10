82d96088bf48a022d10111deee9305777146b333
Harlan T Wood <dev+harlan@comakery.com>
2016-05-10 15:26:39 -0700

    standard token interface

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,18 +1,54 @@
+// Implements standard token interface
+// From: https://github.com/slockit/DAO/blob/f640568e694a057aaeb64a0f1049fae27efe818b/Token.sol
+// See also https://github.com/ethereum/wiki/wiki/Standardized_Contract_APIs
+
 contract TokenInterface {
     mapping (address => uint256) balances;
     mapping (address => mapping (address => uint256)) allowed;
 
+    /// Total amount of tokens
     uint256 public totalSupply;
 
+    /// @param _owner The address from which the balance will be retrieved
+    /// @return The balance
     function balanceOf(address _owner) constant returns (uint256 balance);
+
+    /// @notice Send `_amount` tokens to `_to` from `msg.sender`
+    /// @param _to The address of the recipient
+    /// @param _amount The amount of tokens to be transferred
+    /// @return Whether the transfer was successful or not
     function transfer(address _to, uint256 _amount) returns (bool success);
 
+    /// @notice Send `_amount` tokens to `_to` from `_from` on the condition it
+    /// is approved by `_from`
+    /// @param _from The address of the origin of the transfer
+    /// @param _to The address of the recipient
+    /// @param _amount The amount of tokens to be transferred
+    /// @return Whether the transfer was successful or not
     function transferFrom(address _from, address _to, uint256 _amount) returns (bool success);
+
+    /// @notice `msg.sender` approves `_spender` to spend `_amount` tokens on
+    /// its behalf
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @param _amount The amount of tokens to be approved for transfer
+    /// @return Whether the approval was successful or not
     function approve(address _spender, uint256 _amount) returns (bool success);
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining);
+
+    /// @param _owner The address of the account owning tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @return Amount of remaining tokens of _owner that _spender is allowed
+    /// to spend
+    function allowance(
+        address _owner,
+        address _spender
+    ) constant returns (uint256 remaining);
 
     event Transfer(address indexed _from, address indexed _to, uint256 _amount);
-    event Approval(address indexed _owner,address indexed _spender,uint256 _amount);
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _amount
+    );
 }
 
 /*string public version;   // eg comakery-token-v1.0 */
