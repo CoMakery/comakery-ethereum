2188cbc24019ccadf8db33aaf238b4dbddc43d6e
Harlan T Wood <dev+harlan@comakery.com>
2016-07-26 15:08:10 -0700

    remove extraneous quotes

--- truffle.js
+++ truffle.js
@@ -1,31 +1,30 @@
 /* eslint-disable no-var */
-
 var _ = require('lodash')
 var overrides = {}
 try { overrides = require('./truffle-overrides') } catch (e) {}
 
 const defaults = {
   networks: {
-    'production': {
+    production: {
       network_id: 1, // Ethereum public network
       host: 'localhost',
       port: 9999,
       'ethercamp-subdomain': 'live'
     },
-    'morden': {
+    morden: {
       network_id: 2, // morden
       host: 'localhost',
       port: 8888,
       'ethercamp-subdomain': 'morden-state'
     },
-    'test': {
+    test: {
       network_id: 'default',
       host: 'localhost',
       port: 7777,
       from: '0x7e5f4552091a69125d5dfcb7b8c2659029395bdf',
       'ethercamp-subdomain': 'not-really'
     },
-    'development': {
+    development: {
       network_id: 'default',
       host: 'localhost',
       port: 7777,
@@ -36,7 +35,7 @@ const defaults = {
     // - gasPrice
   },
   mocha: {
-    'useColors': true
+    useColors: true
   }
 }
 
